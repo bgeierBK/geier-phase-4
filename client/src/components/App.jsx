95e6aa0fc42ac5c938d22e173fcb5bec13857773
Victor <victorac2324@gmail.com>
2024-05-21 14:37:19 -0400

    added components

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -2,7 +2,7 @@ function App() {
 
   return (
     <div className='App'>
-      <h1>Hello World</h1>
+      <h1>collector market</h1>
     </div>
   )
 
---
+++ client/src/components/Marketplacecard.jsx
@@ -0,0 +1,7 @@
+function Marketplacecard(){
+    return (
+        <h1> this is where user collection will be </h1>
+    )
+}
+
+export default Marketplacecard
