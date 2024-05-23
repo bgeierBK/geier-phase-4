03d4b88b6b784f8c6eb3eba352a352c853ce5ffd
Victor <victorac2324@gmail.com>
2024-05-23 11:39:49 -0400

    added sign up and userpanel

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,22 +1,37 @@
 import { Outlet } from "react-router-dom"
 import Navbar from "./Navbar"
+import { useEffect, useState } from "react"
 
 function App() {
 
-  const collections = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
-  const users = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 's']
+  const [currentUser, setCurrentUser]= useState(null)
+
+  const [collections, setCollections] =useState([])
+  useEffect(()=>{fetch("http://localhost:3000/collections")
+  .then(res => res.json())
+  .then(collections => setCollections (collections))
+  }, [])
+
+  const [users, setUsers] = useState([])
+  useEffect(()=>{fetch("http://localhost:3000/users")
+  .then(res => res.json())
+  .then(users => setUsers (users))
+  }, [])
+ 
+  // const collections = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+  // const users = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 's']
 
   return (
 
     <div className='App'>
-
+ 
       <h1 style={{
         width: '100%',
         textAlign: 'center',
         color: '#333'
       }}>Collector Market</h1>
         <Navbar/>
-      <Outlet context={{collections:collections, users: users}} />
+      <Outlet context={{collections:collections, users: users, currentUser:currentUser, setCurrentUser:setCurrentUser}} />
      
     </div>
   )
--- client/src/components/Signup.jsx
+++ client/src/components/Signup.jsx
@@ -1,8 +1,35 @@
+import { useState } from "react"
+import { useOutlet, useOutletContext } from "react-router-dom"
 
 
 function Signup(){
 
+const[username, setUsername]= useState ('')
+const[password, setPassword]= useState ('')
 
+const{setCurrentUser}=useOutletContext()
+
+function handleSubmit(e){
+    e.preventDefault()
+    fetch("/api/users", {
+        method : 'POST',
+        headers: {
+            "Content-Type": "application/json"
+
+        },
+        body: JSON.stringify({username, password})
+    })
+    .then (res =>{
+        if(res.ok ){
+            res.json()
+            .then(newUser => setCurrentUser(newUser))
+        }else{
+            alert("Signup Failed")
+        }
+    })
+
+}
+ 
     return(
         <div>
             <h2>Sign up</h2>
@@ -11,8 +38,8 @@ function Signup(){
                 flexWrap: 'wrap',
                 flexDirection: 'column'
             }}>
-                <input type="'text" name="username" placeholder="username" />
-                <input type="password" name="password" placeholder="password" />
+                <input onChange ={e => setUsername(e.target.value)} type="'text" name="username" placeholder="username" value={username} />
+                <input onChange ={e => setPassword(e.target.value)} type="password" name="password" placeholder="password" value={password}/>
                 <input type="submit" name="submit" value='Sign up'/>
             </form>
         </div>
