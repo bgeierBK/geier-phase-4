6816a81ad9caf221262fe8284cf7515855d235a9
Victor <victorac2324@gmail.com>
2024-05-23 10:19:00 -0400

    Navbar add as well as payment form

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,6 +1,16 @@
+import { Link } from "react-router-dom"
+
 function Navbar(){
     return(
-        <h1>this will be nav bar</h1>
+        <div className="nav">
+            <Link to ="/"> Home </Link>
+            <Link to ="/userprofile"> Profile </Link>
+            <Link to ="/marketplace"> Marketplace </Link>
+            <Link to ="/cart"> Your Cart </Link>
+            <Link to ="/checkout"> Checkout </Link>
+
+
+        </div>
     )
 }
 
